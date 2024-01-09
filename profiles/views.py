e5f2276dd2a3f32f3213ba81d5f54f42bbb5ca2b
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 21:22:09 +0000

    feat(api): Add Filter to ProfileDetail class

--- profiles/views.py
+++ profiles/views.py
@@ -34,5 +34,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)).order_by('-created_at')
     serializer_class = ProfileSerializer
