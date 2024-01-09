a5a554a79dbc1c4a04846dad7f04a7c5545aafc5
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 21:54:33 +0000

    feat(api): Install Django all-Auth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -52,6 +52,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken', 
     'dj_rest_auth',
+    'django.contrib.sites', 
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
+    'dj_rest_auth.registration',
     'profiles',
     'posts',
     'comments',
@@ -59,6 +64,8 @@ INSTALLED_APPS = [
     'recommends',
     'followers',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
