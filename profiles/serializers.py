a5bbd0c4aa384fcde3a3ae27fc4e2bee16b115aa
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 21:21:04 +0000

    feat(api): Add Filter to ProfileList class

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,17 +7,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
-    
+
     def get_following_id(self, obj):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if user.is_authenticated:
-            following = Follower.objects.filter(
-                owner=user, followed=obj.owner
-            ).first()
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
             return following.id if following else None
         return None
 
@@ -32,5 +33,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             "content",
             "image",
             "is_owner",
-            'following_id',
+            "following_id",
+            "posts_count",
+            "followers_count",
+            "following_count",
         ]
