2caf1279a313f464af5f4048db5e53f2d52b67d3
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 21:18:38 +0000

    feat(api): Add following id to Profile serializers. Add like id, and recommend id fields to Post serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
+from recommends.models import Recommend
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,11 +9,31 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    like_id = serializers.SerializerMethodField()
+    recommend_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
     
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner = user, post = obj
+            ).first()
+            return like.id if like else None
+        return None
+    
+    def get_recommend_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            recommend = Recommend.objects.filter(
+                owner = user, post = obj
+            ).first()
+            return recommend.id if recommend else None
+        return None
+
     def validate_image(self, value):
         if value.size > 1024 * 1024 *2:
             raise serializers.ValidationError(
@@ -41,4 +63,6 @@ class PostSerializer(serializers.ModelSerializer):
             "content",
             "image",
             "image_filter",
+            'like_id',
+            'recommend_id'
         ]
