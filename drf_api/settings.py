f14e4ba10e1a3ac7e2d8a7ab12dd85c4f569964c
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 22:09:06 +0000

    feat(api): Prepare for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -72,15 +72,25 @@ REST_FRAMEWORK = {
             if "DEV" in os.environ
             else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
         )
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    
 }
+if "DEV" not in os.environ:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "restframework.renderers.JSONRenderer"
+    ]
+
 
 REST_USE_JWT = True
-JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_SECURE = True
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
 
-REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'}
+REST_AUTH_SERIALIZERS = {
+    "USER_DETAILS_SERIALIZER": "drf_api.serializers.CurrentUserSerializer"
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
