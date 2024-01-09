226777ac5b9e5355996a782d35b59ed7925a61c7
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 11:28:28 +0000

    feat(api): Create custom permission authentication and authorisation

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,7 +4,21 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
-        fields = ["id", "owner", "created_at", "updated_at", "name", "content", "image"]
+        fields = [
+            "id",
+            "owner",
+            "created_at",
+            "updated_at",
+            "name",
+            "content",
+            "image",
+            "is_owner",
+        ]
