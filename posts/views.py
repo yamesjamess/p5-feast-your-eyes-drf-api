b7be19d9c0fbc7da17fd7f2ebd6840dd3db8ccff
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-09 21:35:44 +0000

    feat(api): Add Filter to PostDetail class

--- posts/views.py
+++ posts/views.py
@@ -38,4 +38,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        recommends_count=Count('recommends', distinct=True),
+        comments_count=Count('comment', distinct=True)).order_by('-created_at')
