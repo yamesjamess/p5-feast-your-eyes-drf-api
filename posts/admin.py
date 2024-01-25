34c4f29f5ec8486dcd27bcf4d74294cf666be83a
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 11:10:48 +0000

    feat: register model to admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Post
 
-@admin.site.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'created_at', 'restaurant', 'title', 'tag', 'content', 'image')
-    list_display_links = ('id', 'title')
+admin.site.register(Post)
+
