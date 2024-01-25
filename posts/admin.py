ca2540be6bc88ebbab3ee545fe8371b8d27d832f
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 11:08:20 +0000

    feat: register model to admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .model import Post
 
-# Register your models here.
+@admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'created_at', 'restaurant', 'title', 'tag', 'content', 'image')
+    list_display_links = ('id', 'title')
--- recommends/admin.py
+++ recommends/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Recommend
 
-# Register your models here.
+admin.site.register(Recommend)
