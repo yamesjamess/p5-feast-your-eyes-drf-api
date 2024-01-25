9865b550b68a9ea7e33504f5c6dafadf3d80e384
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 11:29:33 +0000

    fix: tutor help fixing settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -146,15 +146,15 @@ WSGI_APPLICATION = "drf_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if "DEV" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+DATABASES = {	
+    'default': ({	
+        'ENGINE': 'django.db.backends.sqlite3',	
+        'NAME': BASE_DIR / 'db.sqlite3',	
+    } if 'DEV' in os.environ else dj_database_url.parse(	
+        os.environ.get('DATABASE_URL')	
+    ))
+}
+    
 
 
 
