0cee41823fd8b5af1895d19abef7f41e5b0c7b50
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-25 10:59:40 +0000

    feat: register model to admin.py

---
+++ drf_api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from posts.models import Post
+from comments.models import Comment
+from likes.models import Like
+from followers.models import Follower
+from profiles.models import Profile
+from recommends.models import Recommend
+
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follower)
+admin.site.register(Recommend)
