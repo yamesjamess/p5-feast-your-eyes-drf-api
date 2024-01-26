8ab35969c93196e8897f9bc4e083e930f242ced5
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-26 22:21:06 +0000

    feat: update admin.py for all models to include more fields

--- likes/admin.py
+++ likes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Like
 
-admin.site.register(Like)
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    """
+    Custom Admin configuration for the Like model.
+    """
+    list_display = ('owner', 'post', 'created_at')
+    list_display_links = ('owner', 'post')
+    list_filter = ('created_at',)
+    search_fields = ('owner', 'post')
