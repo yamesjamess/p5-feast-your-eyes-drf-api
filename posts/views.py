57c08a8185e6a708768f4a30c08aff4aa8d92b4a
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-26 10:23:53 +0000

    fix: revert changes from the test

--- posts/views.py
+++ posts/views.py
@@ -30,7 +30,6 @@ class PostList(generics.ListCreateAPIView):
         "owner__followed__owner__profile",
         "likes__owner__profile",
         "recommends__owner__profile",
-        "posts__tag",
     ]
     ordering_fields = [
         "likes_count",
