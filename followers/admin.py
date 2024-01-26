617cfd6b3f169f39380e32c2857426290f20c644
yamesjamess <jameskasetarapanya15@gmail.com>
2024-01-26 22:20:52 +0000

    fix: fix field values errors

--- followers/admin.py
+++ followers/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Follower
 
 @admin.register(Follower)
-class RecommendAdmin(admin.ModelAdmin):
+class FollowerAdmin(admin.ModelAdmin):
     """
     Custom Admin configuration for the Follower model.
     """
-    list_display = ('owner', 'post', 'created_at')
-    list_display_links = ('owner', 'post')
+    list_display = ('owner', 'followed', 'created_at')
+    list_display_links = ('owner', 'followed')
     list_filter = ('created_at',)
-    search_fields = ('owner', 'post')
+    search_fields = ('owner', 'followed')
